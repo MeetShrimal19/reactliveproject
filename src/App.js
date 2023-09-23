745d9b5428b17612790e468dc74445bb695c5d1f
MeetShrimal19 <shrimalmeet2001@gmail.com>
2023-09-23 16:49:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+//import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import Error from './components/Error';
+
+
+function App() {
+  const [mode, setMode]= useState('light');
+  const [alert, setAlert]= useState(null);
+
+  const showAlert=(message,type)=>
+  {
+      setAlert(
+        {
+          msg: message,
+          type: type
+        }
+      )
+      setTimeout(() => {
+        setAlert(null); 
+      }, 3000);
+  }
+  const toggelMode=()=>
+  {
+    if(mode==='light')
+    {
+      setMode('dark')
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark mode has been enabled", "success");
+      document.title="TextUtils - DarkMode";
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled", "success");
+      document.title="TextUtils - LightMode";
+    }
+  }
+
+  return (
+    <>
+    
+    <Navbar title="TextUtils" about="About" mode={mode} toggelMode={toggelMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    {/* <Routes>
+          <Route exact path="/about" element={ }>
+        
+           
+          </Route>
+          <Route exact path="/" element={}>
+               
+          </Route>
+          <Route path='*' element={<Error/>}>
+        </Route>
+        </Routes> */}
+        <TextForm showAlert={showAlert} heading="Enter the text below"/>
+        {/* <About /> */}
+    </div>
+    
+    </>  
+  );
+}
+
+export default App;
---
+++ src/module2.mjs
@@ -0,0 +1,5 @@
+const a="meet";
+const b= " shalin";
+const c= "rohit";
+const d=" lakshit";
+export default c;
